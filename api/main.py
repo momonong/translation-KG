2eda4ab6eebc781e66f7a0a1e84d7eb6017543b6
Morris <morrischen2.9@gmail.com>
2025-05-22 14:53:42 +0800

    Fix CORS problem

--- api/main.py
+++ api/main.py
@@ -1,8 +1,23 @@
 # api/main.py
 from fastapi import FastAPI
 from api.routers import keywords, related_terms
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+# ✅ 加入這段
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "chrome-extension://kmbpineinpkiobkfoggcaabgonkknnjj"
+    ],  # 你的 Extension ID
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(keywords.router, prefix="/api")
 app.include_router(related_terms.router, prefix="/api")
